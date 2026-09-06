2ec612b2ac744dc4ffcbbb20e757534c8f1f7b87
Suraj Mavuleti <surajmavuleti@gmail.com>
2026-09-06 20:16:03 +0530

    Feat(v8): Visual Overhaul using CustomTkinter Modern UI Framework

--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# AUTO-UPDATER
 cd /home/suraj/.gemini/antigravity/scratch/ultimate_suite/zero-ray-windows
 git pull origin main --quiet
-python3 zero_ray_gui.py
+/home/suraj/.gemini/antigravity/scratch/v8_env/bin/python3 zero_ray_gui.py
